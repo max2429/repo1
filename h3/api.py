39d44885dc51d22dd38751004b9dabb4a483dd40
max2429 <95585351+max2429@users.noreply.github.com>
2024-07-08 16:16:27 +0300

    Add files via upload

---
+++ h3/api.py
@@ -0,0 +1,333 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import abc
+import json
+import datetime
+import logging
+import hashlib
+import uuid
+from optparse import OptionParser
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+from scoring import get_interests, get_score
+
+SALT = "Otus"
+ADMIN_LOGIN = "admin"
+ADMIN_SALT = "42"
+OK = 200
+BAD_REQUEST = 400
+FORBIDDEN = 403
+NOT_FOUND = 404
+INVALID_REQUEST = 422
+INTERNAL_ERROR = 500
+ERRORS = {
+    BAD_REQUEST: "Bad Request",
+    FORBIDDEN: "Forbidden",
+    NOT_FOUND: "Not Found",
+    INVALID_REQUEST: "Invalid Request",
+    INTERNAL_ERROR: "Internal Server Error",
+}
+UNKNOWN = 0
+MALE = 1
+FEMALE = 2
+GENDERS = {
+    UNKNOWN: "unknown",
+    MALE: "male",
+    FEMALE: "female",
+}
+AGE_LIMIT = 70
+
+
+class Field:
+   def __init__(self,required=False,nullable=False):
+      self.required = required
+      self.nullable = nullable
+
+   def validate(self, value):
+      if value is None and self.required:
+         raise ValueError(f'Value {type(self).__name__} is mandatory')
+
+      if value in ('', [], (), {}) and not self.nullable:
+         raise ValueError("Value should not be empty")
+      return value
+
+
+class CharField(Field):
+    def validate(self, value):
+       super().validate(value)
+       if not isinstance(value, str):
+          raise ValueError("Value should be string")
+       return value
+
+
+class ArgumentsField(Field):
+    def validate(self, value):
+       super().validate(value)
+       if not isinstance(value, dict):
+          raise ValueError("Value should be dictionary")
+       return value
+
+
+
+class EmailField(CharField):
+    def validate(self, value):
+       super().validate(value)
+       if '@' not in value:
+          raise ValueError("Value should be valid email")
+       return value
+
+class PhoneField(Field):
+    def validate(self, value):
+        super().validate(value)
+        if not isinstance(value, (int, str)):
+            raise ValueError("The value should be string or integer")
+        if not str(value)[0] == '7' or not len(str(value)) == 11:
+            raise ValueError("Value should start with 7 and has length 11")
+        return value
+
+class DateField(Field):
+    def validate(self, value):
+        super().validate(value)
+        try:
+            datetime.datetime.strptime(value, '%d.%m.%Y')
+        except ValueError:
+            raise ValueError("Value should be date with DD.MM.YYYY format")
+        return value
+
+
+class BirthDayField(Field):
+    def validate(self, value):
+        super().validate(value)
+        birthday = datetime.datetime.strptime(value, '%d.%m.%Y')
+        if datetime.datetime.now() - birthday > datetime.timedelta(
+            days=AGE_LIMIT*365
+        ):
+            raise ValueError(f"Birthday value should be no earlier than {AGE_LIMIT} years ago")
+        return value
+
+
+class GenderField(Field):
+    def validate(self, value):
+        super().validate(value)
+        if not isinstance(value, int) or value not in GENDERS:
+            raise ValueError("Value should be integer 0, 1 or 2")
+        return value
+
+
+class ClientIDsField(Field):
+    def validate(self, value):
+        super().validate(value)
+        if not isinstance(value, list):
+            raise ValueError('Value should be list')
+        for item in value:
+            if not isinstance(item, int):
+                raise ValueError("Value should be list of integers")
+        return value
+
+
+class MetaRequest(type):
+    def __new__(meta, name, bases, attrs):
+        fields = {}
+        for key, value in attrs.items():
+            if isinstance(value, Field):
+                fields[key] = value
+        attrs['_fields'] = fields
+        return type.__new__(meta, name, bases, attrs)
+
+
+class Request(metaclass=MetaRequest):
+    def __init__(self, **kwargs):
+        for attribute in self._fields:
+            value = kwargs.get(attribute)
+            setattr(self, attribute, value)
+
+    def validate(self):
+        for attribute, field in self._fields.items():
+            value = getattr(self, attribute)
+            if value is not None or field.required:
+                field.validate(value)
+
+    def __repr__(self):
+        attributes = {
+            name: getattr(self, name)
+            for name in self.__dict__
+            if name[0:2] != '__'
+        }
+        return f"<Class {self.__class__.__name__}: {attributes}>"
+
+
+class MethodRequest(Request):
+    account = CharField(required=False, nullable=True)
+    login = CharField(required=True, nullable=True)
+    token = CharField(required=True, nullable=True)
+    arguments = ArgumentsField(required=True, nullable=True)
+    method = CharField(required=True, nullable=False)
+
+    @property
+    def is_admin(self):
+        return self.login == ADMIN_LOGIN
+
+
+class ClientsInterestsRequest(MethodRequest):
+    client_ids = ClientIDsField(required=True)
+    date = DateField(required=False, nullable=True)
+
+
+class OnlineScoreRequest(MethodRequest):
+    first_name = CharField(required=False, nullable=True)
+    last_name = CharField(required=False, nullable=True)
+    email = EmailField(required=False, nullable=True)
+    phone = PhoneField(required=False, nullable=True)
+    birthday = BirthDayField(required=False, nullable=True)
+    gender = GenderField(required=False, nullable=True)
+
+    @property
+    def check_fields(self):
+        if (
+            (self.phone and self.email)
+            or (self.first_name and self.last_name)
+            or (self.birthday and self.gender in GENDERS)
+        ):
+            return True
+        else:
+            return False
+
+
+def check_auth(request):
+    if request.is_admin:
+        digest = hashlib.sha512((datetime.datetime.now().strftime("%Y%m%d%H") + ADMIN_SALT).encode('utf-8')).hexdigest()
+    else:
+        digest = hashlib.sha512((request.account + request.login + SALT).encode('utf-8')).hexdigest()
+    if digest == request.token:
+        return True
+    return False
+
+
+def ClientsInterestsHandler(request, ctx, store):
+    try:
+        r = ClientsInterestsRequest(**request.arguments)
+        r.validate()
+    except ValueError as err:
+        return {
+            'code': INVALID_REQUEST,
+            'error': str(err)
+        }, INVALID_REQUEST
+
+    clients_interests = {}
+    for client_id in r.client_ids:
+        clients_interests[f'client_id{client_id}'] = get_interests(
+            'nowhere_store', client_id)
+    return clients_interests, OK
+
+
+def OnlineScoreHandler(request, ctx, store):
+    if request.is_admin:
+        score = 42
+        return {'score': score}, OK
+    try:
+        r = OnlineScoreRequest(**request.arguments)
+        r.validate()
+    except ValueError as err:
+        return {
+            'code': INVALID_REQUEST,
+            'error': str(err)
+        }, INVALID_REQUEST
+
+    if not r.check_fields:
+        return {
+           'code': INVALID_REQUEST,
+           'error': 'INVALID_REQUEST: not enough fields'
+        }, INVALID_REQUEST
+
+    score = get_score(store, r)
+    return {'score': score}, OK
+
+
+def method_handler(request, ctx, store):
+    response, code = None, None
+    method = {'clients_interests': ClientsInterestsHandler,
+              'online_score': OnlineScoreHandler}
+    try:
+        req = MethodRequest(**request.get('body'))
+        req.validate()
+    except ValueError as e:
+        
+        return {
+            'code': INVALID_REQUEST,
+            'error': str(e)
+        }, INVALID_REQUEST
+
+
+    if not req.method:
+        return {
+            'code': INVALID_REQUEST,
+            'error': 'INVALID_REQUEST'
+        }, INVALID_REQUEST
+
+    if not check_auth(req):
+        return None, FORBIDDEN
+
+
+    response, code = method[req.method](req, ctx, store)
+    return response, code
+
+
+class MainHTTPHandler(BaseHTTPRequestHandler):
+    router = {
+        "method": method_handler
+    }
+    store = None
+
+    def get_request_id(self, headers):
+        return headers.get('HTTP_X_REQUEST_ID', uuid.uuid4().hex)
+
+    def do_POST(self):
+        response, code = {}, OK
+        context = {"request_id": self.get_request_id(self.headers)}
+        request = None
+        try:
+            data_string = self.rfile.read(int(self.headers['Content-Length']))
+            request = json.loads(data_string)
+        except:
+            code = BAD_REQUEST
+
+        if request:
+            path = self.path.strip("/")
+            logging.info("%s: %s %s" % (self.path, data_string, context["request_id"]))
+            if path in self.router:
+                try:
+                    response, code = self.router[path]({"body": request, "headers": self.headers}, context, self.store)
+                except Exception as e:
+                    logging.exception("Unexpected error: %s" % e)
+                    code = INTERNAL_ERROR
+            else:
+                code = NOT_FOUND
+
+        self.send_response(code)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        if code not in ERRORS:
+            r = {"response": response, "code": code}
+        else:
+            r = {"error": response or ERRORS.get(code, "Unknown Error"), "code": code}
+        context.update(r)
+        logging.info(context)
+        self.wfile.write(json.dumps(r))
+        return
+
+
+if __name__ == "__main__":
+    op = OptionParser()
+    op.add_option("-p", "--port", action="store", type=int, default=8080)
+    op.add_option("-l", "--log", action="store", default=None)
+    (opts, args) = op.parse_args()
+    logging.basicConfig(filename=opts.log, level=logging.INFO,
+                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+    server = HTTPServer(("localhost", opts.port), MainHTTPHandler)
+    logging.info("Starting server at %s" % opts.port)
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    server.server_close()
